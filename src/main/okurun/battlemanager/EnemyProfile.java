a90924faa470e74e65ee4c576270370482eb5467
Naoki Okumura <naoki.okumura@gmail.com>
2026-06-30 16:44:39 +0900

    EnemyProfile.isAliveをAtomicBooleanに変更

--- src/main/okurun/battlemanager/EnemyProfile.java
+++ src/main/okurun/battlemanager/EnemyProfile.java
@@ -3,12 +3,13 @@ package okurun.battlemanager;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class EnemyProfile {
     private final int id;
     private final Deque<EnemyState> stateHistory = new ConcurrentLinkedDeque<>();
 
-    private volatile boolean isAlive = true;
+    private final AtomicBoolean isAlive = new AtomicBoolean(true);
 
     public EnemyProfile(int id) {
         this.id = id;
@@ -19,12 +20,12 @@ public class EnemyProfile {
     }
 
     public boolean isAlive() {
-        return isAlive;
+        return isAlive.get();
     }
 
     public void died() {
         System.out.println("EnemyProfile.died(): id=" + id);
-        this.isAlive = false;
+        this.isAlive.set(false);
     }
 
     public void addState(EnemyState state) {
